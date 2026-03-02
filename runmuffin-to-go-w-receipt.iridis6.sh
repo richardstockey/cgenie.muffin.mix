1de9a7a328041c99e8af4d7208fdd5388ed31315
richardstockey <50849895+richardstockey@users.noreply.github.com>
2026-03-02 14:47:20 +0000

    updates

---
+++ runmuffin-to-go-w-receipt.iridis6.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+#
+#####################################################################
+### SCIPT TO RUN ./RUNMUFFIN.SH ON REDHAT HPC #######################
+#####################################################################
+module load python/3.12
+module load gcc/13.2.0
+
+LD_LIBRARY_PATH=$HOME/lib
+export LD_LIBRARY_PATH
+
+printf "#!/bin/sh
+
+#SBATCH --nodes=1                # Number of nodes requested
+#SBATCH --time=48:00:00
+#SBATCH --mail-user=$1
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+module load gcc/6.4.0
+module load gnumake
+
+LD_LIBRARY_PATH=$HOME/lib
+export LD_LIBRARY_PATH
+
+cd ~/cgenie.muffin/genie-main
+
+make cleanall &> ~/cgenie_log/cleanall_trash.txt;
+./runmuffin.sh $2 $3 $4 $5 $6 &> ~/cgenie_log/cGEnIE.output_$(date '+%F_%H.%M').log
+" > ~/cgenie.jobs/muffin-to-go.sbatch
+
+sbatch ~/cgenie.jobs/muffin-to-go.sbatch
+#rm ~/cgenie.jobs/muffin-to-go.sbatch
